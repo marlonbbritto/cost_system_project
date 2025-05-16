7563508baf7667accf9b5dd1cd17d46161f372d9
marlonbbritto <marlonbbritto@gmail.com>
2025-05-16 10:59:48 -0300

    feat(TestConfig): insert more information for testing

--- src/main/java/com/costSystemProject/api/config/TestConfig.java
+++ src/main/java/com/costSystemProject/api/config/TestConfig.java
@@ -34,32 +34,83 @@ public class TestConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+
+        CostCenter c1 = CostCenter.builder()
+                .name("Projeto_Teste_A")
+                .description("Projeto de transformação digital Fase A")
+                .build();
+        CostCenter c2 = CostCenter.builder()
+                .name("Manutenção_Sistema_B")
+                .description("Manutenção evolutiva do Sistema B")
+                .build();
+        CostCenter c3 = CostCenter.builder()
+                .name("Inovação_Labs_X")
+                .description("Laboratório de Inovação para novas tecnologias")
+                .build();
+        costCenterRepository.saveAll(List.of(c1, c2, c3));
+
+
         Employee e1 = Employee.builder()
                 .name("João Silva")
                 .position(Position.SOFTWARE_ENGINEER)
                 .level(Level.PLENO)
                 .email("joao.silva@gmail.com")
-                .birthDate(LocalDate.now())
-                .dateOfHiring(LocalDate.now())
+                .birthDate(LocalDate.of(1990, 5, 15))
+                .dateOfHiring(LocalDate.now().minusYears(3))
                 .typeOfContract(TypeOfContract.REMOTE)
-                .salary(5000.11)
-                .valueOfBenefitsCard(1000.00)
-                .valueOfHealthPlans(500.00)
-                .valueOfOtherBenefits(0.00)
+                .salary(5500.50)
+                .valueOfBenefitsCard(1200.00)
+                .valueOfHealthPlans(600.00)
+                .valueOfOtherBenefits(100.00)
                 .build();
-
-        CostCenter c1 = CostCenter.builder()
-                .name("Projeto_Teste")
-                .description("Projeto de transformação digital")
+        Employee e2 = Employee.builder()
+                .name("Maria Oliveira")
+                .position(Position.BUSINESS_ANALYST)
+                .level(Level.SÊNIOR)
+                .email("maria.oliveira@gmail.com")
+                .birthDate(LocalDate.of(1985, 10, 20))
+                .dateOfHiring(LocalDate.now().minusYears(7))
+                .typeOfContract(TypeOfContract.HYBRID)
+                .salary(8200.75)
+                .valueOfBenefitsCard(1500.00)
+                .valueOfHealthPlans(750.00)
+                .valueOfOtherBenefits(250.00)
                 .build();
+        Employee e3 = Employee.builder()
+                .name("Pedro Souza")
+                .position(Position.LEAD_SOFTWARE_ENGINEER)
+                .level(Level.SÊNIOR)
+                .email("pedro.souza@gmail.com")
+                .birthDate(LocalDate.of(1988, 1, 25))
+                .dateOfHiring(LocalDate.now().minusYears(5))
+                .typeOfContract(TypeOfContract.ON_SITE)
+                .salary(9800.90)
+                .valueOfBenefitsCard(1800.00)
+                .valueOfHealthPlans(800.00)
+                .valueOfOtherBenefits(300.00)
+                .build();
+        employeeRepository.saveAll(List.of(e1, e2, e3));
 
-        EmployeeAllocation ea1 = new EmployeeAllocation(null,e1, YearMonth.now(),100.00,c1);
 
-        e1.setAllocations(new ArrayList<>(List.of(ea1)));
-        c1.setEmployeeAllocationList(new ArrayList<>(List.of(ea1)));
+        EmployeeAllocation ea1 = new EmployeeAllocation(null, e1, YearMonth.now(), 70.00, c1);
+        EmployeeAllocation ea2 = new EmployeeAllocation(null, e1, YearMonth.now(), 30.00, c2);
+        EmployeeAllocation ea3 = new EmployeeAllocation(null, e2, YearMonth.now(), 100.00, c1);
+        EmployeeAllocation ea4 = new EmployeeAllocation(null, e3, YearMonth.now(), 50.00, c2);
+        EmployeeAllocation ea5 = new EmployeeAllocation(null, e3, YearMonth.now(), 50.00, c3);
+        employeeAllocationRepository.saveAll(List.of(ea1, ea2, ea3, ea4, ea5));
 
-        costCenterRepository.save(c1);
-        employeeRepository.save(e1);
 
+        e1.setAllocations(new ArrayList<>(List.of(ea1, ea2)));
+        e2.setAllocations(new ArrayList<>(List.of(ea3)));
+        e3.setAllocations(new ArrayList<>(List.of(ea4, ea5)));
+
+        c1.setEmployeeAllocationList(new ArrayList<>(List.of(ea1, ea3)));
+        c2.setEmployeeAllocationList(new ArrayList<>(List.of(ea2, ea4)));
+        c3.setEmployeeAllocationList(new ArrayList<>(List.of(ea5)));
+
+        employeeRepository.saveAll(List.of(e1, e2, e3));
+        costCenterRepository.saveAll(List.of(c1, c2, c3));
+
+        System.out.println("### Dados de teste criados com sucesso! ###");
     }
 }
