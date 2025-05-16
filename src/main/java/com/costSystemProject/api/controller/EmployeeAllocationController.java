875ba52c9c068cccab692d50e61e9f6e25fc1a55
Marlon Barbosa Britto <marlon.britto@db1.com.br>
2025-05-16 17:12:16 -0300

    feat(EmployeeAllocation): insert find by id

--- src/main/java/com/costSystemProject/api/controller/EmployeeAllocationController.java
+++ src/main/java/com/costSystemProject/api/controller/EmployeeAllocationController.java
@@ -8,10 +8,12 @@ import com.costSystemProject.api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/employees_allocation")
@@ -24,5 +26,10 @@ public class EmployeeAllocationController {
     public ResponseEntity<List<EmployeeAllocationDto>> findAll(){
         List<EmployeeAllocationDto> employeeAllocationDtoList = employeeAllocationService.findAll();
         return ResponseEntity.ok().body(employeeAllocationDtoList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<EmployeeAllocationDto> findById(@PathVariable UUID id){
+        return employeeAllocationService.findById(id);
     }
 }
--- src/main/java/com/costSystemProject/api/service/EmployeeAllocationService.java
+++ src/main/java/com/costSystemProject/api/service/EmployeeAllocationService.java
@@ -4,9 +4,11 @@ import com.costSystemProject.api.domain.employeeAllocation.EmployeeAllocation;
 import com.costSystemProject.api.dto.EmployeeAllocationDto;
 import com.costSystemProject.api.repository.EmployeeAllocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +22,13 @@ public class EmployeeAllocationService {
         return employeeAllocations.stream()
                 .map(this::convertToDto)
                 .collect(Collectors.toList());
+    }
+
+    public ResponseEntity<EmployeeAllocationDto> findById(UUID id){
+        return employeeAllocationRepository.findById(id)
+                .map(this::convertToDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     private EmployeeAllocationDto convertToDto(EmployeeAllocation allocation){
